11cd2814d70886104b1989982d43ead12c21f7ab
Khristine Ramilo <kramilo2@my.centennialcollege.ca>
2026-06-03 13:28:20 -0400

    Fix another link

--- src/App.js
+++ src/App.js
@@ -41,7 +41,7 @@ const App = () => {
 
         {/* Right Side */}
         <div className="footer-socials">
-            <a href="https://github.com" target="_blank" rel="noreferrer">GitHub</a>
+            <a href="https://github.com/..." target="_blank" rel="noopener noreferrer">GitHub</a>
             <a href="https://khristine-ramilo-portfolio.netlify.app/" target="_blank" rel="noreferrer">Live Site</a>
         </div>
     </div>
